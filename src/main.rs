0a7991ff1ffdedb24d5e22a8971e6e9ed10b67b8
itsara <khajer@gmail.com>
2026-01-20 00:04:09 +0700

    add log info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,17 @@
+use tracing::info;
+use tracing_subscriber::fmt;
+
+fn setup_logging() {
+    fmt()
+        .with_target(false)
+        .with_max_level(tracing::Level::INFO)
+        .init();
+}
+
 #[tokio::main]
 async fn main(){
-    println!("Hello, world!");
+    setup_logging();
+    let host = "127.0.0.1:6772";
+
+    info!("Starting server at {}", host);
 }
