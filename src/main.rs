5db015c3732bd49502f69047c6287a8442c3fc60
itsara <khajer@gmail.com>
2026-01-20 16:54:44 +0700

    add udp server listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tracing::info;
 use tracing_subscriber::fmt;
+use std::io;
+use tokio::net::UdpSocket;
 
 fn setup_logging() {
     fmt()
@@ -9,9 +11,21 @@ fn setup_logging() {
 }
 
 #[tokio::main]
-async fn main(){
+async fn main() -> io::Result<()> {
     setup_logging();
     let host = "127.0.0.1:6772";
 
     info!("Starting server at {}", host);
+    let sock = UdpSocket::bind(host).await?;
+
+    let mut buf = [0; 1024];
+    loop {
+
+        let (len, addr) = sock.recv_from(&mut buf).await?;
+        println!("Received {} bytes from {}", len, addr);
+
+        sock.send_to(&buf[..len], addr).await?;
+
+    }
+
 }
